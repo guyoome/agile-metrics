720838abb3753d2a7a5cd356fdc92584f490cf83
guyoome <guillaumemaurin.gm@gmail.com>
2022-01-13 18:02:21 +0100

    add multi team

--- src/HealthCheck/GlobalHealthCheck.js
+++ src/HealthCheck/GlobalHealthCheck.js
@@ -86,37 +86,59 @@ function HealthCheck() {
     const [dataTable, setDataTable] = useState([]);
     const [errorJson, setErrorJson] = useState();
     const [team, setTeam] = useState("<team name>");
-    const [teams, setTeams] = useState([{ name: "", result: "", json: "" }])
+    const [teams, setTeams] = useState([{ name: "", result: [], json: "" }])
+
+    const [update, setUpdate] = useState();
 
 
-    // useEffect(() => {
-    //     console.log("🎈teams:", teams);
+    useEffect(() => {
+        console.log("🎈UPDATE");
+        try {
+            // const json = teams[0].json;
+            setCategories(getCategories(teams[0].json));
 
-    // }, [JSON.stringify(teams)]);
+            setErrorJson();
+        } catch (error) {
+            setErrorJson(error.message)
+            console.log("💫", error.message)
+        }
 
-    // useEffect(() => {
-    //     try {
-    //         setCategories(getCategories(json));
-    //         setErrorJson();
-    //     } catch (error) {
-    //         setErrorJson(error.message)
-    //         console.log("💫", error.message)
-    //     }
+    }, [update]);
 
-    // }, [json])
+    useEffect(() => {
+        console.log("🎈Result#1", teams);
+        console.log("🎈Result#2", categories);
 
-    // useEffect(() => {
-    //     try {
-    //         setDataTable(getDataTable(categories, json));
-    //         // setDataTable(getResults(categories, json));
-    //         setErrorJson();
-    //     } catch (error) {
-    //         setDataTable([]);
-    //         setErrorJson(error.message);
-    //         console.log("💫", error.message);
-    //     }
+    }, [categories])
 
-    // }, [categories])
+    useEffect(() => {
+        console.log("🎃categories:", categories)
+        try {
+            // setDataTable(getDataTable(categories, json));
+            const newTeams = teams;
+            // for (let index = 0; index < newTeams.length; index++) {
+                // const element = array[index];
+                // newTeams[index].result = getDataTable(categories, newTeams.json).result;
+                // }
+                teams.forEach((team, id) => {
+                    // newTeams[id].result = getDataTable(categories, newTeams[id].json);
+                    const dataTable = getDataTable(categories, newTeams[id].json);
+                    const result = dataTable;
+                    console.log("😋result", result)
+
+                newTeams[id].result = result;
+            });
+            setTeams(newTeams);
+            setUpdate();
+            console.log("🎏newTeams:", newTeams)
+            setErrorJson();
+        } catch (error) {
+            setDataTable([]);
+            setErrorJson(error.message);
+            console.log("💫", error.message);
+        }
+
+    }, [categories])
 
     const setName = (id, e) => {
         const newTeams = teams;
@@ -133,39 +155,41 @@ function HealthCheck() {
     }
 
     return (
-        <div className='container --grid-1'>
-            {/* <div className='left'> */}
-            {/* <p>🔗<a href='https://metroretro.io/board/LBPH2U7G29TC' target="_blank">Link to MetroRetro template</a></p> */}
-            <div>
-                <button
-                    onClick={() => { setTeams(teams.concat({ name: "", result: "", json: "" })) }}>
-                    <span className='emoji'>➕</span> Add a team</button>
-                <div className='mt-5'></div>
-                <button
-                    onClick={() => { setTeams(teams.slice(0, -1)) }}>
-                    <span className='emoji'>➖</span> Remove last team</button>
-                <div className='mt-5'></div>
-                <button>
-                    <span className='emoji'>🎉</span> Generate Table</button>
-            </div>
-            {teams.map((team, id) => (
-                <div key={id}>
-                    <input type="text" placeholder='Team Name'
-                        onChange={(e) => { setName(id, e) }}></input>
+        // <div className='container --grid-1'>
+        <div>
+            <div className='container --grid-1'>
+                {/* <p>🔗<a href='https://metroretro.io/board/LBPH2U7G29TC' target="_blank">Link to MetroRetro template</a></p> */}
+                <div>
+                    <button
+                        onClick={() => { setTeams(teams.concat({ name: "", result: [], json: "" })) }}>
+                        <span className='emoji'>➕</span> Add a team</button>
                     <div className='mt-5'></div>
-                    <textarea rows="5" cols="33"
-                        placeholder='Export MetroRetro to JSON'
-                        onChange={(e) => {
-                            try { setJson(id, e); setErrorJson() }
-                            catch (error) { setErrorJson(error.message); setDataTable([]); }
-                        }}>
-                    </textarea>
-                    <p style={{ color: "red" }}>{errorJson ? "Invalid JSON" : ""}</p>
+                    <button
+                        onClick={() => { setTeams(teams.slice(0, -1)) }}>
+                        <span className='emoji'>➖</span> Remove last team</button>
+                    <div className='mt-5'></div>
+                    <button
+                        onClick={(e) => setUpdate(e)}>
+                        <span className='emoji'>🎉</span> Generate Table</button>
                 </div>
-            ))}
+                {teams.map((team, id) => (
+                    <div key={`input-${id}`}>
+                        <input type="text" placeholder='Team Name'
+                            onChange={(e) => { setName(id, e) }}></input>
+                        <div className='mt-5'></div>
+                        <textarea rows="5" cols="33"
+                            placeholder='Export MetroRetro to JSON'
+                            onChange={(e) => {
+                                try { setJson(id, e); setErrorJson() }
+                                catch (error) { setErrorJson(error.message); setDataTable([]); }
+                            }}>
+                        </textarea>
+                        <p style={{ color: "red" }}>{errorJson ? "Invalid JSON" : ""}</p>
+                    </div>
+                ))}
 
 
-            {/* </div> */}
+            </div>
             <div className='right'>
                 <Button.SaveAsPNG icon="💾" text="Download Table" node="agile-health-check-table" fileName="agile-health-check-table" />
 
@@ -174,24 +198,26 @@ function HealthCheck() {
                     <table >
                         <thead>
                             <tr>
-                                <th>{team}blabla</th>
-                                <th>Red <span className='emoji'>🔴</span></th>
-                                <th>Orange <span className='emoji'>🟠</span></th>
-                                <th>Green <span className='emoji'>🟢</span></th>
-                                <th>Trend (<span className='emoji'>🔽</span>/<span className='emoji'>⏸</span>/<span className='emoji'>🔼</span>)</th>
-                                <th>Result</th>
+                                <th>{team}</th>
+                                {teams.map((team, id) => (
+                                    <th key={`th-${id}`}>{team.name}</th>
+                                ))}
+                                {/* <th>Result</th> */}
                             </tr>
                         </thead>
 
                         <tbody className='am-data-table__content'>
-                            {dataTable.map((element, i) => (
+                            {categories.map((category, i) => (
                                 <tr key={i}>
-                                    <td>{element.category}</td>
-                                    <td>{element.red}</td>
+                                    <td>{category}</td>
+                                    {teams.map((team, id)=>(
+                                        <td key={id}>{team.result[i] ? team.result[i].result : ""}</td>
+                                    ))}
+                                    {/* <td>{element.red}</td>
                                     <td>{element.orange}</td>
                                     <td>{element.green}</td>
                                     <td>{element.down}/{element.stable}/{element.up}</td>
-                                    <td>{element.result}</td>
+                                    <td>{element.result}</td> */}
                                 </tr>
                             ))}
                         </tbody>
